e91fb96981301953d59fac5fe110e979bd8e06db
Jimmy Bowcott <151764691+JimmyBowcott@users.noreply.github.com>
2025-03-28 20:58:04 +0000

    add remote repo macro

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,21 @@
 vim.g.snacks_animate = false
 vim.api.nvim_set_keymap('n', '<leader>h', ':Dashboard<CR>', { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>yf", ":let @+ = expand('%:p')<CR>", { desc = "Yank Filepath" })
+
+vim.api.nvim_create_user_command("Repo", function()
+  local handle = io.popen("git remote get-url origin")
+  if handle then
+    local url = handle:read("*a"):gsub("%s+$", "")
+    handle:close()
+
+    if url == "" then
+      print("No remote URL found")
+      return
+    end
+
+    url = url:gsub("^git@([^:]+):(.+)$", "https://%1/%2")
+
+    local open_cmd = vim.fn.has("win32") == 1 and "start" or "xdg-open"
+    os.execute(open_cmd .. " " .. url)
+  end
+end, { desc = "Open remote repository" })
