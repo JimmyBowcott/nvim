b5e418116edff2d1f3c7df871e7eeda9aae88b3c
Jimmy <JimmyBowcott@users.noreply.github.com>
2025-04-13 21:34:45 +0100

    <leader>oe -> open in explorer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,4 +3,14 @@
 -- Add any additional keymaps here
 local wk = require("which-key")
 
+-- Open remote repo
 wk.add({ "<leader>gr", "<cmd>Repo<CR>", desc = "Open Remote" })
+
+-- Open file in explorer
+vim.keymap.set("n", "<leader>oe", function()
+    local path = vim.fn.expand("%:p:h")
+    local cmd = vim.fn.has("win32") == 1 and 'start "" "' .. path .. '"' or
+    vim.fn.has("mac") == 1 and 'open "' .. path .. '"' or
+    'xdg-open "' .. path .. '"'
+    vim.fn.system(cmd)
+end, { desc = "Open current file in explorer" })
